79c96546889e1c2894cba728a417048542e857ae
Fabio Vedovelli <vedovelli@gmail.com>
2022-01-15 10:10:10 +0100

    Add cover picture in the post page

--- web/app/features/Blog/components/Post/Post.tsx
+++ web/app/features/Blog/components/Post/Post.tsx
@@ -4,6 +4,8 @@ import { DateTimeUtils } from '~/util';
 import { Post } from '../../types';
 import m2r from 'minutes-to-read';
 import { ContentUtils } from '../Content';
+import pictures from '~/features/Blog/components/Posts/pictures-space.json';
+import shuffle from 'lodash/shuffle';
 
 export interface PostProps {
   post: Post;
@@ -13,35 +15,45 @@ export interface PostProps {
 export function Post({ post, preview = false }: PostProps) {
   const content = ContentUtils.blocksToText(post.content).join(' ');
   const minutesToRead = m2r(content);
+  const [picture] = shuffle(pictures);
   return (
-    <Prose>
-      <>
-        {preview ? (
-          <div className="p-2 antialiased font-semibold text-center bg-opacity-70 text-normal bg-light-snow-storm1 text-dark-polar-night3">
-            Preview Mode Enabled
+    <>
+      <div className="w-full h-40 md:h-60">
+        <img
+          src={picture.regular}
+          className="object-cover w-full h-full"
+          alt={`${post.title} - Cover image`}
+        />
+      </div>
+      <Prose>
+        <>
+          {preview ? (
+            <div className="p-2 antialiased font-semibold text-center bg-opacity-70 text-normal bg-light-snow-storm1 text-dark-polar-night3">
+              Preview Mode Enabled
+            </div>
+          ) : null}
+          <Headings.Content>{post.title}</Headings.Content>
+          <div className="pb-5 text-sm border-b text-light-snow-storm3 border-dark-polar-night1">
+            <p>
+              By <span className="font-semibold">{post.author}</span> -{' '}
+              <Link.External href={`https://${post.author_url}`}>{post.author_url}</Link.External>
+            </p>
+            <dl>
+              <dd>Created: {DateTimeUtils.date(post._createdAt)}</dd>
+            </dl>
+            <dl>
+              <dd>Last update: {DateTimeUtils.date(post._updatedAt)}</dd>
+            </dl>
+            <dl>
+              <dd className="inline-block mt-6 text-base">
+                ⏳ &nbsp;&nbsp;{minutesToRead}, not taking into consideration the code examples.
+              </dd>
+            </dl>
           </div>
-        ) : null}
-        <Headings.Content>{post.title}</Headings.Content>
-        <div className="pb-5 text-sm border-b text-light-snow-storm3 border-dark-polar-night1">
-          <p>
-            By <span className="font-semibold">{post.author}</span> -{' '}
-            <Link.External href={`https://${post.author_url}`}>{post.author_url}</Link.External>
-          </p>
-          <dl>
-            <dd>Created: {DateTimeUtils.date(post._createdAt)}</dd>
-          </dl>
-          <dl>
-            <dd>Last update: {DateTimeUtils.date(post._updatedAt)}</dd>
-          </dl>
-          <dl>
-            <dd className="inline-block mt-6 text-base">
-              ⏳ &nbsp;&nbsp;{minutesToRead}, not taking into consideration the code examples.
-            </dd>
-          </dl>
-        </div>
-        <Content blocks={post.content} />
-        <Comments />
-      </>
-    </Prose>
+          <Content blocks={post.content} />
+          <Comments />
+        </>
+      </Prose>
+    </>
   );
 }
