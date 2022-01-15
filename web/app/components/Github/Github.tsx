49344bb40f81f95afa9248faa2d99d04542ffb5b
Fabio Vedovelli <vedovelli@gmail.com>
2022-01-15 15:51:41 +0100

    Introduces Github top menu with items
    Plus some improvements in the Feed component

--- web/app/components/Github/Github.tsx
+++ web/app/components/Github/Github.tsx
@@ -2,9 +2,9 @@ import { Fragment } from 'react';
 import { Menu, Transition } from '@headlessui/react';
 import classNames from 'classnames';
 import { Icon, Icons } from '~/components';
-import { domain } from '~/config';
+import { domain, siteName } from '~/config';
 
-export function Feed() {
+export function Github() {
   return (
     <Menu as="div" className="relative ml-1">
       <div>
@@ -13,7 +13,7 @@ export function Feed() {
           className="flex text-sm bg-gray-800 rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
         >
           <span className="sr-only">Open user menu</span>
-          <Icon icon={Icons.feed} className="w-6 h-6 text-light-snow-storm4" />
+          <Icon icon={Icons.github} className="w-6 h-6 text-light-snow-storm4" />
         </Menu.Button>
       </div>
       <Transition
@@ -25,49 +25,36 @@ export function Feed() {
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95"
       >
-        <Menu.Items className="absolute right-0 z-20 w-24 py-1 mt-2 origin-top-right bg-white rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+        <Menu.Items className="absolute right-0 z-20 py-1 mt-2 origin-top-right bg-white rounded-md shadow-lg w-60 ring-1 ring-black ring-opacity-5 focus:outline-none">
           <Menu.Item>
             {({ active }) => (
               <a
-                href={`https://${domain}/rss.xml`}
-                className={classNames(
-                  active ? 'bg-gray-100' : '',
-                  'block px-4 py-2 text-sm text-gray-700'
-                )}
+                href={`https://github.com/${siteName}/${domain}`}
                 target="_blank"
                 rel="noopener noreferrer"
+                className={classNames(
+                  active ? 'bg-gray-100' : '',
+                  'px-4 py-2 text-sm text-gray-700 flex gap-2'
+                )}
               >
-                RSS
+                <Icon icon={Icons.github} className="w-5 h-5 text-gray-700" />
+                This blog's source code
               </a>
             )}
           </Menu.Item>
           <Menu.Item>
             {({ active }) => (
               <a
-                href={`https://${domain}/atom.xml`}
-                className={classNames(
-                  active ? 'bg-gray-100' : '',
-                  'block px-4 py-2 text-sm text-gray-700'
-                )}
+                href={`https://github.com/${siteName}`}
                 target="_blank"
                 rel="noopener noreferrer"
-              >
-                Atom
-              </a>
-            )}
-          </Menu.Item>
-          <Menu.Item>
-            {({ active }) => (
-              <a
-                href={`https://${domain}/feed.json`}
                 className={classNames(
                   active ? 'bg-gray-100' : '',
-                  'block px-4 py-2 text-sm text-gray-700'
+                  'px-4 py-2 text-sm text-gray-700 flex gap-2'
                 )}
-                target="_blank"
-                rel="noopener noreferrer"
               >
-                JSON
+                <Icon icon={Icons.github} className="w-5 h-5 text-gray-700" />
+                Our organization
               </a>
             )}
           </Menu.Item>
