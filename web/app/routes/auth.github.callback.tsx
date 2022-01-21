5cf74bcaeccba220d08b5420cff312c2bc551e63
Fabio Vedovelli <vedovelli@gmail.com>
2022-01-21 08:31:09 +0100

    Finish redirect upon login

--- web/app/routes/auth.github.callback.tsx
+++ web/app/routes/auth.github.callback.tsx
@@ -1,10 +1,13 @@
 import type { LoaderFunction } from 'remix';
 import { route } from 'routes-gen';
+import { parseCookie, signinRedirectCookie } from '~/cookies';
 import { auth } from '~/services/auth.server';
 
-export const loader: LoaderFunction = async ({ request, params }) => {
+export const loader: LoaderFunction = async ({ request }) => {
+  const cookie = await parseCookie(request, signinRedirectCookie);
+
   return auth.authenticate('github', request, {
-    successRedirect: route('/'),
+    successRedirect: `/${cookie}` ?? '/',
     failureRedirect: route('/'),
   });
 };
--- web/app/routes/auth.github.tsx
+++ web/app/routes/auth.github.tsx
@@ -1,32 +1,29 @@
 import type { ActionFunction } from 'remix';
-import { route } from 'routes-gen';
 import { auth } from '~/services/auth.server';
+import { signinRedirectCookie } from '../cookies';
 
 export const action: ActionFunction = async ({ request }) => {
   const data = Object.fromEntries(await request.formData());
 
-  // async function login(request: Request) {
-  //   let url = new URL(request.url);
-  //   let returnTo = url.searchParams.get("returnTo") as string | null;
-  //   try {
-  //     return await auth.authenticate("auth0", request, {
-  //       successRedirect: returnTo ?? "/home",
-  //       failureRedirect: "/",
-  //     });
-  //   } catch (error) {
-  //     if (!returnTo) throw error;
-  //     if (error instanceof Response && isRedirect(error)) {
-  //       error.headers.append(
-  //         "Set-Cookie",
-  //         await returnToCookie.serialize(returnTo)
-  //       );
-  //       return error;
-  //     }
-  //     throw error;
-  //   }
-  // }
-  return await auth.authenticate('github', request, {
-    successRedirect: route('/:slug', { slug: data.slug as string }),
-    failureRedirect: route('/'),
-  });
+  const returnTo = String(data.slug ?? '');
+  try {
+    return await auth.authenticate('github', request, {
+      successRedirect: returnTo ?? '/',
+      failureRedirect: '/',
+    });
+  } catch (error) {
+    // error is a Response which is vital for the flow to continue
+    if (!returnTo) throw error;
+    if (error instanceof Response) {
+      error.headers.append(
+        'Set-Cookie',
+        await signinRedirectCookie.serialize(returnTo, {
+          httpOnly: true,
+        })
+      );
+
+      return error;
+    }
+    throw error;
+  }
 };
