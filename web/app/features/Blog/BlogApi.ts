bdc9a06cfadfc503c31bfa93d7ccccbf2b791916
Fábio Vedovelli <vedovelli@gmail.com>
2022-01-26 18:03:42 +0100

    Prevent draft posts to be displayed (#13)

--- web/app/features/Blog/BlogApi.ts
+++ web/app/features/Blog/BlogApi.ts
@@ -3,16 +3,18 @@ import { getClient } from '~/lib/sanity/getClient';
 import { BlogTypes } from '.';
 
 export function getPosts({ language }: { language: string }): Promise<BlogTypes.Post[]> {
-  const query = `*[_type=='post'  && language == '${
+  const query = `*[!(_id in path('drafts.**')) && _type=='post'  && language == '${
     language ?? defaultLanguage
   }' && type == 'post'] | order(_createdAt desc)`;
 
   return getClient().fetch(query);
 }
 
-export function getPost(slug?: string) {
-  const query = `*[_type == "post" && slug.current ==  $slug]`;
+export function getPost(slug?: string, preview = false) {
   const queryParams = { slug };
+  const query = preview
+    ? `*[_type == "post" && slug.current ==  $slug]`
+    : `*[!(_id in path('drafts.**')) && _type == "post" && slug.current ==  $slug]`;
 
   return getClient().fetch(query, queryParams);
 }
