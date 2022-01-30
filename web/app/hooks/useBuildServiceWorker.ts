d566412305969d5116358423e8f09e5c8de5c992
Fabio Vedovelli <vedovelli@gmail.com>
2022-01-30 10:57:52 +0100

    Improve offline support with the Remix PWA project

---
+++ web/app/hooks/useBuildServiceWorker.ts
@@ -0,0 +1,40 @@
+import { useLocation, useMatches } from 'remix';
+import { useEffect } from 'react';
+
+let isMount = true;
+
+export function useBuildServiceWorker() {
+  const matches = useMatches();
+  const location = useLocation();
+
+  useEffect(() => {
+    let mounted = isMount;
+    isMount = false;
+    if ('serviceWorker' in navigator) {
+      if (navigator.serviceWorker.controller) {
+        navigator.serviceWorker.controller?.postMessage({
+          type: 'REMIX_NAVIGATION',
+          isMount: mounted,
+          location,
+          matches,
+          manifest: window.__remixManifest,
+        });
+      } else {
+        let listener = async () => {
+          await navigator.serviceWorker.ready;
+          navigator.serviceWorker.controller?.postMessage({
+            type: 'REMIX_NAVIGATION',
+            isMount: mounted,
+            location,
+            matches,
+            manifest: window.__remixManifest,
+          });
+        };
+        navigator.serviceWorker.addEventListener('controllerchange', listener);
+        return () => {
+          navigator.serviceWorker.removeEventListener('controllerchange', listener);
+        };
+      }
+    }
+  }, [location]);
+}
