6f673f0d521610dc8d49933b3bc1c1e4faa1d4fd
Fabio Vedovelli <vedovelli@gmail.com>
2022-02-20 11:13:03 +0100

    Update remix.config.js

--- web/remix.config.js
+++ web/remix.config.js
@@ -5,9 +5,4 @@ module.exports = {
   serverBuildTarget: 'netlify',
   server: './server.js',
   ignoredRouteFiles: ['.*'],
-  // appDirectory: "app",
-  // assetsBuildDirectory: "public/build",
-  // serverBuildPath: "netlify/functions/server/index.js",
-  // publicPath: "/build/",
-  // devServerPort: 8002
 };
